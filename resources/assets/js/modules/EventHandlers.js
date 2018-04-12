0caabbf254b48e335b41d2e28b3fe31e1282da8c
mateusz_fludra <mateusz.fludra@gmail.com>
2018-04-12 23:42:45 +0200

    mobile navbar resized

--- resources/assets/js/modules/EventHandlers.js
+++ resources/assets/js/modules/EventHandlers.js
@@ -11,33 +11,6 @@ export default class EventHandlers
                     $(".navbar__list").toggleClass('navbar_show');
                 }
             },
-            // {
-            //     $el: $('header'),
-            //     event: "mousemove",
-            //     handler: function(e) {
-            //         let header = $('header');
-            //         let text = $('header > h1');
-            //
-            //         let width = header[0].offsetWidth;
-            //         let height = header[0].offsetHeight;
-            //         let walk = 10;
-            //
-            //         let x = e.offsetX;
-            //         let y = e.offsetY;
-            //
-            //         if (this !== e.target) {
-            //             x = x + e.target.offsetLeft;
-            //             y = y + e.target.offsetTop;
-            //         }
-            //
-            //         let xWalk = Math.round((x / width * walk) - (walk / 3.5));
-            //         let yWalk = Math.round((y / height * walk) - (walk / 3.5));
-            //
-            //         let textShadow = xWalk + 'px '+ yWalk + 'px ' + '0 rgba(167, 207, 243, .7)';
-            //
-            //         text.css('textShadow', textShadow);
-            //     }
-            // },
             {
                 $el: $('.recaptcha'),
                 event: "validation",
@@ -46,7 +19,7 @@ export default class EventHandlers
                         $(this).children().last().css('display', 'none');
                     }
                 }
-            }
+            },
         ];
 
         this.bindEventHandlers();
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -7,7 +7,7 @@
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', {{env('GOOGLE_ANALYTICS')}});
+        gtag('config', '{{env('GOOGLE_ANALYTICS')}}');
     </script>
     @yield('title')
     <meta charset="utf-8">
